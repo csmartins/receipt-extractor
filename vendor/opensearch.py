69b8d0b5e746f30217203729808f2ebefdf6d1e0
Cadu Martins <csmartins.personal@gmail.com>
2022-09-28 00:04:48 -0300

    cluster and client for opensearch

---
+++ vendor/opensearch.py
@@ -0,0 +1,37 @@
+from opensearchpy import OpenSearch
+
+def save_to_opensearch(host, port, user, password, product):
+
+    client = OpenSearch(
+        hosts = [{'host': host, 'port': port}],
+        http_compress = True, # enables gzip compression for request bodies
+        http_auth = (user, password),
+        # client_cert = client_cert_path,
+        # client_key = client_key_path,
+        use_ssl = True,
+        verify_certs = False,
+        ssl_assert_hostname = False,
+        ssl_show_warn = False,
+        # ca_certs = ca_certs_path
+    )
+
+    # Create an index with non-default settings.
+    index_name = 'products'
+    # index_body = {
+    #     'settings': {
+    #         'index': {
+    #         'number_of_shards': 4
+    #         }
+    #     }
+    # }
+    # print('\nCreating index:') 
+    # response = client.indices.create(index_name, body=index_body)
+    # print(response)
+
+    print("Save product to Opensearch")
+    response = client.index(
+        index = index_name,
+        body = product,
+        refresh = True
+    )
+    print(response)
